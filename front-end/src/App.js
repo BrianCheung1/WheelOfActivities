b5fd49a08151d223a5303376a7f96a84f6b69003
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2023-06-23 22:47:17 -0400

    added backend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,15 @@ import * as React from "react"
 import Navigationbar from "./components/Navbar"
 import Wheel from "./components/Wheel"
 import SpinWheel from "./components/SpinWheel"
+import wheelsService from "./services/wheels"
 
 const App = () => {
   return (
     <div>
       <Navigationbar />
-      <Wheel />
+      {/* <Wheel /> */}
       <SpinWheel />
+      
     </div>
   )
 }
--- front-end/src/components/SpinWheel.js
+++ front-end/src/components/SpinWheel.js
@@ -1,53 +1,125 @@
 import React, { useState } from "react"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import Container from "react-bootstrap/Container"
+import Modal from "react-bootstrap/Modal"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+const Notification = ({ show, handleClose, winner }) => {
+  return (
+    <Modal show={show} onHide={handleClose} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Winner</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{winner}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
 
 const Wheel = () => {
-  const slices = ["1", "2", "3"]
+  const [slices, setSlices] = useState(["a", "b", "c"])
   const [spinning, setSpinning] = useState(false)
   const [randomAngle, setRandomAngle] = useState(0)
+  const [winner, setWinner] = useState("")
+  const [show, setShow] = useState(false)
+  const [turning, setTurning] = useState(false)
+  const handleClose = () => setShow(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const activity = event.target.activity.value
+    setSlices(slices.concat(activity))
+    event.target.activity.value = ""
+  }
 
   const spinWheel = () => {
-    const newRandomAngle = Math.floor(Math.random() * 360)
+    const newRandomAngle = Math.floor(Math.random() * 360 * 10)
     console.log(newRandomAngle)
     setRandomAngle(newRandomAngle)
     setSpinning(false)
+    setTurning(true)
     // Delay the re-application of animation to ensure the reset animation is complete
     setTimeout(() => {
       setSpinning(true)
     }, 100)
+    setWinner(
+      slices.at(
+        (Math.ceil(newRandomAngle / (360 / slices.length)) % slices.length) - 1
+      )
+    )
+    setTimeout(() => {
+      setShow(true)
+      setTurning(false)
+    }, 3100)
   }
 
   return (
-    <div>
-      <div
-        className={`circle ${spinning ? "spinning" : ""}`}
-        style={{ "--randomAngle": `${randomAngle}deg` }}
-      >
-        {slices.map((slice, index) => (
-          <li
-            key={index}
-            style={{
-              transform: `rotate(${(360 / slices.length) * index}deg) skewY(${
-                360 / slices.length - 90
-              }deg)`,
-            }}
+    <Container fluid>
+      <Row className="text-center justify-content-center">
+        <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label>Activity</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Activity"
+                id="activity"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Add Activity
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto"> 
+          <div className="arrow"></div>
+        </Col>
+      </Row>
+      <Row className="circle-container text-center justify-content-center">
+        <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+          <div
+            className={`circle ${spinning ? "spinning" : ""}`}
+            style={{ "--randomAngle": `${randomAngle}deg` }}
           >
-            <div
-              className="text"
-              style={{
-                transform: ` skewY(${
-                  (360 / slices.length - 90) * -1
-                }deg) rotate(${360 / slices.length / 2}deg) `,
-              }}
-            >
-              {slice}
-            </div>
-          </li>
-        ))}
-      </div>
-      <button onClick={spinWheel}>Spin the Wheel</button>
-      <h2>Congratulations!</h2>
-      <p>You landed on: {randomAngle % slices.length}</p>
-    </div>
+            {slices.toReversed().map((slice, index) => (
+              <div
+                className="slice"
+                key={index}
+                style={{
+                  transform: `rotate(${(360 / slices.length) * index}deg)`,
+                  // backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+                }}
+              >
+                <div
+                  className="text"
+                  style={{
+                    transform: ` rotate(${360 / slices.length / 2}deg)`,
+                  }}
+                >
+                  {slice}
+                </div>
+              </div>
+            ))}
+          </div>
+        </Col>
+      </Row>
+      <Row className="text-center justify-content-center">
+        <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+          <Button onClick={spinWheel} disabled={turning}>
+            Spin the Wheel
+          </Button>
+          <Notification winner={winner} handleClose={handleClose} show={show} />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
