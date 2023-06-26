23999592ae9ac67e528aa6dc139e79f2bb352634
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2023-06-26 12:47:13 -0400

    added user creation and login to backend

--- back-end/app.js
+++ back-end/app.js
@@ -3,9 +3,12 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const wheelsRouter = require("./controller/wheel")
+const usersRouter = require("./controller/users")
+const loginRouter = require("./controller/login")
 const middleware = require("./utils/middleware")
 const logger = require("./utils/logger")
 const mongoose = require("mongoose")
+require("express-async-errors")
 
 mongoose.set("strictQuery", false)
 
@@ -21,12 +24,12 @@ mongoose
   })
 
 app.use(cors())
-app.use(express.static("build"))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use("/api/wheels", wheelsRouter)
-
+app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- back-end/controller/wheel.js
+++ back-end/controller/wheel.js
@@ -1,62 +1,57 @@
 const wheelsRouter = require("express").Router()
 const Wheel = require("../models/wheel")
+const { userExtractor } = require("../utils/middleware")
 
 wheelsRouter.get("/", async (request, response) => {
-  const wheels = await Wheel.find({})
+  const wheels = await Wheel.find({}).populate("user", { username: 1, name: 1 })
   response.json(wheels)
 })
 
-wheelsRouter.post("/", (request, response, next) => {
-  const body = request.body
-  if (body.content === undefined) {
-    return response.status(400).json({ error: "content mising" })
-  }
-  const wheel = new Wheel({
-    content: body.content,
-  })
-
-  wheel
-    .save()
-    .then((savedWheel) => {
-      response.json(savedWheel)
-    })
-    .catch((error) => next(error))
-})
-
-wheelsRouter.get("/:id", (request, response, next) => {
-  Wheel.findById(request.params.id)
-    .then((wheel) => {
-      if (wheel) {
-        response.json(wheel)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch((error) => {
-      next(error)
-    })
-})
-
-wheelsRouter.delete("/:id", (request, response, next) => {
-  Wheel.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(204).end()
-    })
-    .catch((error) => next(error))
-})
-
-wheelsRouter.put("/:id", (request, response, next) => {
+wheelsRouter.post("/", userExtractor, async (request, response, next) => {
   const { content } = request.body
 
-  Wheel.findByIdAndUpdate(
+  const wheel = new Wheel({
+    content: content,
+  })
+
+  const user = request.user
+
+  if (!user) {
+    return response.status(401).json({ error: "operation not permitted" })
+  }
+  wheel.user = user._id
+
+  const savedWheel = await wheel.save()
+  user.wheels = user.wheels.concat(savedWheel._id)
+  await user.save()
+  response.json(savedWheel)
+})
+
+wheelsRouter.get("/:id", async (request, response, next) => {
+  const wheel = await Wheel.findById(request.params.id)
+  response.json(wheel)
+})
+
+wheelsRouter.delete("/:id", userExtractor, async (request, response, next) => {
+  const wheel = await Wheel.findById(request.params.id)
+  const user = request.user
+  if (!user || blog.user.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: "Operation not permitted" })
+  }
+  user.wheels = user.wheels.filter((w) => w.toString() !== wheel.id.toString())
+  await user.save()
+  await wheel.remove()
+  response.status(204).end()
+})
+
+wheelsRouter.put("/:id", async (request, response, next) => {
+  const { content } = request.body
+  const updatedWheel = await blog.findByIdAndUpdate(
     request.params.id,
     { content },
-    { new: true, runValidators: true, context: "query" }
+    { new: true }
   )
-    .then((updatedWheel) => {
-      response.json(updatedWheel)
-    })
-    .catch((error) => next(error))
+  response.json(updatedWheel)
 })
 
 module.exports = wheelsRouter
